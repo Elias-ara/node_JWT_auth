a6c726ac30b711167ab2cc33d3fa4e6f6f9a698d
Elias-miranda-DEV <eliasmirandaaraujo8@gmail.com>
2026-01-03 20:05:21 -0300

    Implementation of login route and JWT token.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import bcrypt from "bcryptjs";
 import Fastify from "fastify";
+import jwt from "jsonwebtoken";
 
 const app = Fastify({ logger: true });
+
+const JWT_SECRET = "SuperPassword123";
 
 interface User {
   id: number;
@@ -10,25 +13,31 @@ interface User {
   password: string;
 }
 
+//Criacao do banco de dados em memoria
 const users: User[] = [];
 
+// Rota para testar se o servidor esta funcionando
 app.get("/", async () => {
   return { users };
 });
 
+// Rota para registrar um novo usuario
 app.post("/users/register", async (request, reply) => {
   const { username, email, password } = request.body as {
     username: string;
     email: string;
     password: string;
   };
+  // Verifica se o usuario ja existe
   const user = users.find((u) => u.email === email);
   if (user) {
     return reply.status(409).send({ message: "User already exists" });
   }
 
+  //encripta a senha do usuario
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  // Cria um novo usuario
   const newUser: User = {
     id: users.length + 1,
     username,
@@ -40,6 +49,34 @@ app.post("/users/register", async (request, reply) => {
   return reply.status(201).send({ message: "User registered successfully" });
 });
 
+// Rota para login do usuario
+app.post("/users/login", async (request, reply) => {
+  const { username, email, password } = request.body as {
+    username: string;
+    email: string;
+    password: string;
+  };
+
+  // Verifica se o usuario existe
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return reply.status(401).send({ message: "invalid credentials" });
+  }
+
+  // Verifica se a senha esta correta
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return reply.status(401).send({ message: "invalid credentials" });
+  }
+
+  // Gera um token JWT
+  const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  return { token };
+});
+
+// Inicia o servidor
 const start = async () => {
   try {
     await app.listen({ port: 3333 });
